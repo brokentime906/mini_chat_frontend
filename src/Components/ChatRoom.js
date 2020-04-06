fbd8e45b45957e6408203f759f39a8f6c83f9269
brokentime906 <brokentime906@gmail.com>
2020-04-06 19:29:24 +0900

    oh..

--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -18,7 +18,7 @@ const ChatRoom = ({ chats, setChats, socket, socketLoading }) => {
     e.preventDefault();
     console.log("axios onSubmit 고고");
     message.setValue("");
-    await axios.post("https://mini-chat-mongo.herokuapp.com//chat", {
+    await axios.post("https://mini-chat-mongo.herokuapp.com/chat", {
       chat: message.value,
     });
   };
