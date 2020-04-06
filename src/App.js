444638a87135b80aa5a4b74a6b050d62d911411d
brokentime906 <brokentime906@gmail.com>
2020-04-06 18:45:03 +0900

    First deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socketio from "socket.io-client";
+import ChatRoom from "./Components/ChatRoom";
+import axios from "axios";
 function App() {
+  const [chats, setChats] = useState([]);
+  const [socket, setSocket] = useState(null);
+  const [socketLoading, setSocketLoading] = useState(true);
+  useEffect(() => {
+    setSocketLoading(true);
+    const fetchData = async () => {
+      const { data: _chats } = await axios.get(
+        "https://mini-chat-mongo.herokuapp.com/"
+      );
+      console.log("chats", _chats);
+      setChats(_chats);
+      const _socket = socketio.connect(
+        "https://mini-chat-mongo.herokuapp.com/"
+      );
+      setSocket(_socket);
+      setSocketLoading(false);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ChatRoom
+          chats={chats}
+          setChats={setChats}
+          socket={socket}
+          socketLoading={socketLoading}
+        />
       </header>
     </div>
   );
---
+++ src/Components/ChatRoom.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
+import socketio from "socket.io-client";
+import useInput from "../Hooks/useInput";
+import axios from "axios";
+const ChatRoomBlock = styled.div``;
+
+const ChatRoom = ({ chats, setChats, socket, socketLoading }) => {
+  const message = useInput("");
+  useEffect(() => {
+    if (!socketLoading) {
+      socket.on("newChat", (chat) => {
+        console.log("이게 chat이다", chat);
+        setChats((prev) => [...prev, chat]);
+      });
+    }
+  }, [socketLoading]);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("axios onSubmit 고고");
+    message.setValue("");
+    await axios.post("chat", { chat: message.value });
+  };
+  return (
+    <ChatRoomBlock>
+      <div>ChatRoom Title</div>
+      {chats && chats.map((chat) => <div>{chat.chat}</div>)}
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder={"메세지 입력해라"}
+          value={message.value}
+          onChange={message.onChange}
+        />
+        <button type="submit">MSG 전송</button>
+      </form>
+    </ChatRoomBlock>
+  );
+};
+export default ChatRoom;
